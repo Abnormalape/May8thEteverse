2e6364539b9ee73a9c79ee239fa360ebdbb78c85
Abnormalape <164149628+Abnormalape@users.noreply.github.com>
2024-05-08 15:26:41 +0900

    5월 8일 유니티 2일차 오후-2 - 이동과 회전과 고정축

--- Assets/Scripts/CustomCube.cs
+++ Assets/Scripts/CustomCube.cs
@@ -10,6 +10,9 @@ public class CustomCube : MonoBehaviour
     private float xSpeed = 1f;
     [SerializeField]
     private float zSpeed = 1f;
+
+    [SerializeField] // 초당 360도
+    private float rSpeed = 360f;
     void Start()
     {
         Debug.Log("start"); // 시작
@@ -29,8 +32,11 @@ public class CustomCube : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         //Debug.Log(horizontal);//C#의 모든 개체는 다 toString을 가지고 있다.
         RefTransform.Translate(new Vector3(xSpeed * Time.deltaTime * horizontal, 0 , zSpeed * Time.deltaTime * vertical)); // 프레임 마다 이동
+                                                                                                                           // RefTransform.position = RefTransform.position + new Vector3(0f,0f,horizontal * Time.deltaTime); <- 위치를 이전 위치에서 조금더 이동한다.
 
-        
+        // 단체 주석 ctrl + k -> ctrl + c
+
+        RefTransform.Rotate(new Vector3(0f, - vertical * rSpeed * Time.deltaTime, 0f));
 
     }
 }
