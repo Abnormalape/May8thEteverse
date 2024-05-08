bae1c8d37174e18d2f9d820c6f23c25a250c2ffe
Abnormalape <164149628+Abnormalape@users.noreply.github.com>
2024-05-08 14:21:41 +0900

    5월 8일 유니티 오후 2차 - 방향과 크기와 시간 timedeltatime

--- Assets/Scripts/CustomCube.cs
+++ Assets/Scripts/CustomCube.cs
@@ -7,7 +7,9 @@ public class CustomCube : MonoBehaviour
     [SerializeField]
     private Transform RefTransform;
     [SerializeField]
-    private float xSpeed = 0.01f;
+    private float xSpeed = 1f;
+    [SerializeField]
+    private float zSpeed = 1f;
     void Start()
     {
         Debug.Log("start"); // 시작
@@ -21,6 +23,14 @@ public class CustomCube : MonoBehaviour
     void Update()
     {
         Debug.Log("update"); // 업데이트
-        RefTransform.Translate(new Vector3(xSpeed, 0f, 0f)); // 프레임 마다 이동
+
+        //방향키를 입력받아 이동하려면?
+        float horizontal = Input.GetAxis("Horizontal"); // projectsettings -> inputmanager
+        float vertical = Input.GetAxis("Vertical");
+        //Debug.Log(horizontal);//C#의 모든 개체는 다 toString을 가지고 있다.
+        RefTransform.Translate(new Vector3(xSpeed * Time.deltaTime * horizontal, 0 , zSpeed * Time.deltaTime * vertical)); // 프레임 마다 이동
+
+        
+
     }
 }
