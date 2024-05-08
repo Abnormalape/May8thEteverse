b068aa216224730a7a363aca55677538eefdad79
Abnormalape <164149628+Abnormalape@users.noreply.github.com>
2024-05-08 13:30:27 +0900

    유니티 2일차 오후-1
    - 간단한 이동, 스크립트에 transform을 활용한 이동기능 추가

--- Assets/Scripts/CustomCube.cs
+++ Assets/Scripts/CustomCube.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CustomCube : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Transform RefTransform;
+    [SerializeField]
+    private float xSpeed = 0.01f;
     void Start()
     {
-        Debug.Log("start");
+        Debug.Log("start"); // 시작
+
+        //RefTransform = this.GetComponent<Transform>();
+        RefTransform = transform;
+        RefTransform.Translate(new Vector3(1f,0f,0f));
     }
 
     
     void Update()
     {
-        Debug.Log("update");
+        Debug.Log("update"); // 업데이트
+        RefTransform.Translate(new Vector3(xSpeed, 0f, 0f)); // 프레임 마다 이동
     }
 }
