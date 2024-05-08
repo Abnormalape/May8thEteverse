94fc349d4b1df09cdd2ec6c25aca7f8923bb266e
Abnormalape <164149628+Abnormalape@users.noreply.github.com>
2024-05-08 11:31:29 +0900

    5월 8일 유니티 수업 2일차 - 오전시간

---
+++ Assets/Scripts/CustomCube.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCube : MonoBehaviour
+{
+    
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    
+    void Update()
+    {
+        Debug.Log("update");
+    }
+}
