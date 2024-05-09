e9e44223e2ac4a79f9f1214c71b1778637778110
Abnormalape <164149628+Abnormalape@users.noreply.github.com>
2024-05-09 11:35:12 +0900

    5월 9일 3일차 오전
    - 아이템 프리팹 생성 및 Item 태그 적용
    - 플레이어에서 아이템과 충돌하는 기능 추가함

--- Assets/PLayerMovement.cs
+++ Assets/PLayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
 using UnityEngine.UIElements;
 
 public class PLayerMovement : MonoBehaviour
@@ -23,6 +24,12 @@ public class PLayerMovement : MonoBehaviour
         direction.Normalize();
         this.transform.position = this.transform.position
                 + direction * speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space)){ rb.AddForce(0, 5f, 0, ForceMode.Impulse); }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (other.tag == "Item") { Destroy(other.gameObject); }
     }
 }
